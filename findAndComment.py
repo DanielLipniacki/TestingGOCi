ea0e2e6c036e187d2bed72946cae3d5bbac6c287
Daniel <daniel.lipniacki@codilime.com>
2021-09-07 08:55:16 +0200

    added unit tests

--- findAndComment.py
+++ findAndComment.py
@@ -1,17 +1,32 @@
 import re
+import xml.etree.ElementTree as ET
 
-def findText(text: str):
+def findAndPrintTestCoverage(text: str):
     matches = re.findall('(?<=">TestingGOCi)(.*)(?=<\/option>)', text)
-    print('{"body":" ## Test Coverage\\n | File        | Coverage           |\\n | :-------------: |:-------------:|\\n', end="")
+    print('## Test Coverage\\n | File        | Coverage           |\\n | :-------------: |:-------------:|\\n', end="")
     for match in matches:
         line = match.split(" ")
         print("| " + line[0]+ " | " + line[1][1:-1] + " |\\n", end="")
-    print('"}', end="")
+
+
+def parseUnitTestXml(fileName: str):
+    total : int = 0
+    failures: int = 0
+    mytree = ET.parse(fileName)
+    myroot = mytree.getroot()
+    for testSuite in myroot.findall("testsuite"):
+        total += int(testSuite.get("tests"))
+        failures += int(testSuite.get("failures"))
+    
+    print(f"## Unit Tests\\n Total: {total}, failures: {failures}\\n", end="")
 
 def main():
     coverFile = open("cover.html")
     text = coverFile.read()
     coverFile.close()
-    findText(text)
+    print('{"body":" ', end="")
+    findAndPrintTestCoverage(text)
+    parseUnitTestXml("unit-tests.xml")
+    print('"}', end="")
 
 main()
